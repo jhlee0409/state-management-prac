347fbb1119b6352cfa48a7ac717f1adc9d441344
jack <jacklee@hexconiac.com>
2022-06-15 16:16:40 +0900

    feat: React-Query 셋팅 완료

---
+++ pages/_app.tsx
@@ -0,0 +1,48 @@
+import "@/styles/globals.css";
+import "@/styles/reset.css";
+import { themeClass } from "@/styles/theme.css";
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { NextComponentType } from "next";
+import { AppContext, AppInitialProps, AppProps } from "next/app";
+import { Hydrate } from "react-query/hydration";
+import { ReactQueryDevtools } from "react-query/devtools";
+import LayoutComponent from "@/components/layout/layout";
+
+const MyApp: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
+  Component,
+  pageProps,
+}) => {
+  const queryClientRef = React.useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
+  return (
+    <>
+      <QueryClientProvider client={queryClientRef.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <LayoutComponent>
+            <Component {...pageProps} />
+          </LayoutComponent>
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
+  );
+};
+
+MyApp.getInitialProps = async ({
+  Component,
+  ctx,
+}: AppContext): Promise<AppInitialProps> => {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return { pageProps };
+};
+
+export default MyApp;

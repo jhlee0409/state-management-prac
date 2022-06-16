c22d43cc12bc7d753cea217e4d83256a172583e0
Jack <jhlee409@naver.com>
2022-06-16 22:09:49 +0900

    feat: add default font-familly

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,20 @@ export default class CustomDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link href="http://fonts.cdnfonts.com/css/branche" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
         <body>
           <div id="modal-root" />
           <Main />

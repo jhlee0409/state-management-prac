9843ebc3d18ab994b32ef5b6a69a1a03d8fcf4a8
Jack <jhlee409@naver.com>
2022-07-10 14:00:17 +0900

    fix: layout css

--- src/components/layout/style.css.ts
+++ src/components/layout/style.css.ts
@@ -2,8 +2,11 @@ import { vars } from "@/styles/theme.css";
 import { globalStyle, style } from "@vanilla-extract/css";
 
 export const layout = style({
-  maxWidth: 1200,
   width: "100%",
+});
+
+globalStyle(`${layout} > div`, {
+  maxWidth: 1200,
   margin: "0 auto",
   padding: "40px 40px",
 });
@@ -22,4 +25,10 @@ export const ListLayoutWrapper = style({});
 
 globalStyle(`${ListLayoutWrapper} > h1`, {
   textTransform: "uppercase",
+});
+
+export const detailLayoutWrapper = style({
+  display: "flex",
+  gap: 10,
+  flexDirection: "column",
 });
